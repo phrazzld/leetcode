743fb295e164a61f27546ef5fab5f5948c718640
Phaedrus <phraznikov@gmail.com>
2019-04-01 16:06:24 -0400

    Initial commit

---
+++ javascript/jewels_and_stones.js
@@ -0,0 +1,18 @@
+// 771. Jewels and Stones
+//
+// You're given strings `J` representing the types of stones that are jewels,
+// and `S` representing the stones you have. Each character in `S` is a type of
+// stone you have. You want to know how many of the stones you have are also jewels.
+//
+// The letters in `J` are guaranteed distinct, and all characters in `J` and `S` are
+// letters. Letters are case sensitive, so "a" is considered a different type of
+// stone from "A".
+
+const numJewelsInStones = (J, S) => {
+  let jewels = new Set(J.split(''))
+  let count = 0
+  for (let i = 0; i < S.length; i++) {
+    if (jewels.has(S[i])) count++
+  }
+  return count
+}
