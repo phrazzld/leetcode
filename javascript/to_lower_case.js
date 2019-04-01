cf279001c3db2d7a7e7db190e9f63ae9de097389
Phaedrus <phraznikov@gmail.com>
2019-04-01 16:11:24 -0400

    Solve ToLowerCase (but not cleverly)

---
+++ javascript/to_lower_case.js
@@ -0,0 +1,8 @@
+// 709. To Lower Case
+//
+// Implement function ToLowerCase() that has a string parameter str,
+// and returns the same string in lowercase.
+
+const toLowerCase = str => {
+  return str.toLowerCase()
+}
